9fa62a0ac9ed3211cfabf139a46f7dbba91abc59
Adriana Ito <ito.dri@gmail.com>
2021-02-17 14:18:04 +0900

    finishing controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'destroying restaurants...'
+Restaurant.destroy_all
+
+puts 'creating restaurants...'
+10.times do
+  new_rest = Restaurant.new(
+    name: Faker::JapaneseMedia::OnePiece.character,
+    address: Faker::JapaneseMedia::OnePiece.island,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+    )
+  new_rest.save
+end
+
+puts "Created #{Restaurant.count} restaurants!"
